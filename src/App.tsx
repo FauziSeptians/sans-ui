8bf3e1fea726bb576c2c3f4bc3a2658e7200075b
FauziSeptians <fauzibelajar96@gmail.com>
2025-09-20 18:27:18 +0700

    feat: component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
+import { Text, Title, Typography } from "@lib/main";
 import "./App.css";
 import "./index.css";
-// import { CardComponent } from "zsans-ui";
-import { CardComponent } from "../lib/Card/CardComponent";
 
 function App() {
    return (
       <>
-         <div className="p-3 ">test</div>
-         <CardComponent
-            Header={"Youtuber"}
-            Image={
-               "https://www.lapakgaming.com/blog/id-id/wp-content/uploads/2023/07/windah-basudara-1.webp"
-            }
-            Description={"ini yotuuber"}
-         />
+         <Typography.Text underline type="danger">
+            adwawadawawdww
+         </Typography.Text>
+         <Title size="large">Hallo</Title>
+         <Text bold type="success">
+            wakwawww
+         </Text>
+         <Text highlightText="ning">Warning</Text>
       </>
    );
 }
